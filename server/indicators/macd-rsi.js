de51bfefc8ea4891b1df6b877012ee3a0aee20b8
Antonio Kaplan <antonio@innovent.tv>
2018-02-18 17:55:14 -0800

    add rsi

--- server/indicators/macd-rsi.js
+++ server/indicators/macd-rsi.js
@@ -12,16 +12,22 @@
 // hist_colorChange = input(true,title="MacD Histogram 4 Colors?")
 */
 
-import { sma, ema } from 'technicalindicators';
+import { sma, ema, rsi } from 'technicalindicators';
 import dataForge  from 'data-forge';
 import path from 'path';
 
 /** load custom helper methods **/
-import loadCsv from '../services/csv';
+import {loadCsv,writeCsv} from '../services/csv';
 import Vector from '../services/matrix';
 
 const BASE_PATH = process.env.DATA_DIRECTORY || path.resolve(process.env.PWD);
 
+/**
+ * [fileToDataFrame description]
+ * @param  {[type]} tickerSymbol [description]
+ * @param  {[type]} fileDate     [description]
+ * @return {[type]}              [description]
+ */
 const fileToDataFrame = (tickerSymbol, fileDate) => {
   const filedir = `${BASE_PATH}/data-files/${tickerSymbol}`;
   const filepath = `${tickerSymbol}-${fileDate}.tsv`;
@@ -29,19 +35,20 @@ const fileToDataFrame = (tickerSymbol, fileDate) => {
   return df;
 };
 
+const dataFrameToFile = (tickerSymbol, fileDate, dataFrame) => {
+  const filedir = `${BASE_PATH}/data-files/${tickerSymbol}`;
+  const filepath = `${tickerSymbol}-${fileDate}.csv`;
+  const df = writeCsv(`${filedir}/${filepath}`,dataFrame);
+  console.log(`writing file - ${filedir}/${filepath}`)
+  return df;
+};
+
 const dataframe = fileToDataFrame("XLMBTC", "2018-01-31_");
 
-// var columns = dataframe.getColumns().where(column => column.name == "close");
-// var data = columns.toArray();
-// var columnSubset = df.subset(["Some-Column", "Some-Other-Column"]);
 const closes = dataframe
                 .parseFloats('close')
                 .getSeries('close')
                 .toArray();
-
-// console.log("DATAFRAME-",dataframe.getColumnNames());
-console.log("DATAFRAME rows-", closes.length);
-console.log("DATAFRAME-",typeof closes[4]);
 
 /**
   * define constants for speeds of calculations
@@ -54,22 +61,50 @@ const rsiLength = 10;
 
 // /** calculate exponential moving average as vector **/
 const fastMA = ema({period : fastLength, values : closes})
-console.log("fastMA",fastMA.length);
 const slowMA = ema({period : slowLength, values : closes})
-console.log("slowMA",slowMA.length);
+
 
 const fastMA_slice = fastMA.slice((slowLength-fastLength));
 const macd = Vector.subtractVectors(fastMA_slice, slowMA);
-console.log("macd length",macd.length );
+
 
 const signal = sma({period : signalLength, values : macd})
-console.log("signal length",signal.length );
+const rsi_data = rsi({period : rsiLength, values : macd})
+
 
 const macd_slice = macd.slice(Math.abs(signal.length - macd.length));
 const hist = Vector.subtractVectors(macd_slice, signal);
-console.log("hist",hist.length);
+
+// const indicators = [signal, hist, macd, rsi_data];
+
+const indicators = [signal, hist, macd, rsi_data].map( indicator => Array(closes.length - indicator.length)
+                                                                      .fill(indicator[0])
+                                                                      .concat(indicator));
+
+const indicatorSeries = indicators.map( indicator => new dataForge.Series(indicator));
+
+// const empty_cells_signal = Array(closes.length - signal.length).fill(signal[0]);
+// const empty_cells_hist = Array(closes.length - hist.length).fill(hist[0]);
+// const empty_cells_macd = Array(closes.length - macd.length).fill(macd[0]);
+// const empty_cells_rsi = Array(closes.length - rsi_data.length).fill(rsi_data[0]);
 
 
+// pad array of empty cells with the first value and create Series
+const signal_series = new dataForge.Series(empty_cells_signal.concat(signal));
+const hist_series = new dataForge.Series(empty_cells_hist.concat(hist));
+const macd_series = new dataForge.Series(empty_cells_hist.concat(macd));
+const rsi_series = new dataForge.Series(empty_cells_rsi.concat(rsi_data));
+
+const final_df = dataframe
+                    .withSeries({
+                      macd: macd_series,
+                      macdSignal: signal_series,
+                      macdHist: hist_series,
+                      rsi: rsi_series,
+                    });
+// console.log(final_df.head(10).toString())
+// console.log(final_df.tail(10).toString())
+dataFrameToFile("XLMBTC", "2macd2018-01-31_",final_df);
 //
 // outMacD = security(tickerid, res, macd)
 // outSignal = security(tickerid, res, signal)
